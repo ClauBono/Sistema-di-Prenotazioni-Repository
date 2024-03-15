e6ac0949d754c2ba4e77e9e30bc554c8e2a069b4
ClauBono <120019860+ClauBono@users.noreply.github.com>
2024-03-15 12:16:03 +0100

    Delete src directory

--- src/main/java/StagistiLinks/Sistema/di/Prenotazioni/Repositories/PrenotazioniRepository.java
+++
@@ -1,9 +0,0 @@
-package StagistiLinks.Sistema.di.Prenotazioni.Repositories;
-import StagistiLinks.Sistema.di.Prenotazioni.Entities.PrenotazioniEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PrenotazioniRepository extends JpaRepository <PrenotazioniEntity, Long> {
-}
-
